fe0e87681b72da7e3db6f285081f4fb45c93b46c
sean <sleepypooka@gmail.com>
2014-09-12 15:20:13 -0400

    Updated GAmod.jl, added some preliminary comparison simulations.

--- Diss.jl
+++ Diss.jl
@@ -183,58 +183,58 @@ end
 
 
 # Component update with regularized Sigma
-function update(G::GaussND,x,Sigma,nu)
-    conf = [];
-    conf = conf/sum(conf);
-    G.sigma += (((x-G.mu)*transpose(x-G.mu))*conf[1]+Sigma*conf[2])*G.kappa/(G.kappa+1);
-    G.mu = G.mu*(G.kappa/(G.kappa+1)) + x*(1/(G.kappa+1));
-    G.kappa += 1;
-    G.nu += 1;
-end
-
-
-# Mixture update with regularized Sigma
-function update(M::ItterMix,x,comp::Int=1,reg::Int)
-    # determine if new or old category
-    # p(new) decreases as stick-length decreases
-    isnew = rand(Bernoulli(M.stick));
-    
-    # generate posterior predictive densities
-    # currently terribly clunky
-    
-    if !bool(isnew)
-        post_pred = {MvTDist(float(G.nu-M.d+1),G.mu,G.sigma*((G.kappa+1)/(G.kappa*(G.nu-M.d-1)))) for G in M.components};
-        # Calculate and normalize cluster responsibilities
-        resp = [M.phi[i]*pdf(post_pred[i],x) for i=1:length(post_pred)];
-        resp = float(resp/sum(resp));
-        if comp == 1
-            winner = indmax(resp);
-        else
-            winner = indmax(rand(Multinomial(1,resp)));
-        end
-        mu_temp = M.components[winner].mu
-        kappa_temp = M.components[winner].kappa
-        update(M.components[winner],x,M.mean_sigma,M.ms_nu);
-        mean_sigma += ((x-mu_temp)*transpose(x-mu_temp))*kappa_temp/(kappa_temp+1);
-        ms_nu += 1
-        M.alpha[winner] += 1;
-    else
-        w_new = rand(Beta(1,M.a))*M.stick;
-        M.stick = M.stick - w_new;
-        if M.components == {}
-            sigma_new = M.mean_sigma*(M.alpha[1]);
-            M.components = cat(1,M.components,{GaussND(M.d,x,sigma_new,M.alpha[1],M.alpha[1]+M.d+ms_nu)});
-        else
-            # Calculating phi for new category:
-            # un-normalize current vector phi (divide by 1-current stick length)
-            phi_un = M.phi/(1-M.stick);
-            phi_un = cat(1,phi_un,{w_new});
-            alpha_new = (w_new/sum(phi_un))*sum(M.alpha);
-            M.alpha = cat(1,M.alpha,{alpha_new});
-            sigma_new = M.mean_sigma*(alpha_new);
-            M.components = cat(1,M.components,{GaussND(M.d,x,sigma_new,alpha_new+M.d,alpha_new+M.d+ms_nu)});
-        end
-        
-    end
-    M.phi = {M.alpha[i]/sum(M.alpha) for i=1:length(M.alpha)};
-end
+# function update(G::GaussND,x,Sigma,nu)
+#     conf = [];
+#     conf = conf/sum(conf);
+#     G.sigma += (((x-G.mu)*transpose(x-G.mu))*conf[1]+Sigma*conf[2])*G.kappa/(G.kappa+1);
+#     G.mu = G.mu*(G.kappa/(G.kappa+1)) + x*(1/(G.kappa+1));
+#     G.kappa += 1;
+#     G.nu += 1;
+# end
+# 
+# 
+# # Mixture update with regularized Sigma
+# function update(M::ItterMix,x,comp::Int=1,reg::Int)
+#     # determine if new or old category
+#     # p(new) decreases as stick-length decreases
+#     isnew = rand(Bernoulli(M.stick));
+#     
+#     # generate posterior predictive densities
+#     # currently terribly clunky
+#     
+#     if !bool(isnew)
+#         post_pred = {MvTDist(float(G.nu-M.d+1),G.mu,G.sigma*((G.kappa+1)/(G.kappa*(G.nu-M.d-1)))) for G in M.components};
+#         # Calculate and normalize cluster responsibilities
+#         resp = [M.phi[i]*pdf(post_pred[i],x) for i=1:length(post_pred)];
+#         resp = float(resp/sum(resp));
+#         if comp == 1
+#             winner = indmax(resp);
+#         else
+#             winner = indmax(rand(Multinomial(1,resp)));
+#         end
+#         mu_temp = M.components[winner].mu
+#         kappa_temp = M.components[winner].kappa
+#         update(M.components[winner],x,M.mean_sigma,M.ms_nu);
+#         mean_sigma += ((x-mu_temp)*transpose(x-mu_temp))*kappa_temp/(kappa_temp+1);
+#         ms_nu += 1
+#         M.alpha[winner] += 1;
+#     else
+#         w_new = rand(Beta(1,M.a))*M.stick;
+#         M.stick = M.stick - w_new;
+#         if M.components == {}
+#             sigma_new = M.mean_sigma*(M.alpha[1]);
+#             M.components = cat(1,M.components,{GaussND(M.d,x,sigma_new,M.alpha[1],M.alpha[1]+M.d+ms_nu)});
+#         else
+#             # Calculating phi for new category:
+#             # un-normalize current vector phi (divide by 1-current stick length)
+#             phi_un = M.phi/(1-M.stick);
+#             phi_un = cat(1,phi_un,{w_new});
+#             alpha_new = (w_new/sum(phi_un))*sum(M.alpha);
+#             M.alpha = cat(1,M.alpha,{alpha_new});
+#             sigma_new = M.mean_sigma*(alpha_new);
+#             M.components = cat(1,M.components,{GaussND(M.d,x,sigma_new,alpha_new+M.d,alpha_new+M.d+ms_nu)});
+#         end
+#         
+#     end
+#     M.phi = {M.alpha[i]/sum(M.alpha) for i=1:length(M.alpha)};
+# end
--- GAmod.jl
+++ GAmod.jl
@@ -13,24 +13,26 @@ type GAMix
     components
 end
 
-function MixInit(d::Int,N::Int,lims,dist="normal")
+function MixInit(d::Int,N::Int,lims,dist="normal",scale = 0.2)
     phi = ones(N)/N
     out = GAMix(d,phi,{})
     if dist == "normal"
         mu_dist = MvNormal(lims,eye(d)*3)
+    end
     if dist == "uniform"
         dims = {Uniform(dim[1],dim[2]) for dim in lims}
     end
     for i=1:N
-        if dist = "normal"
+        if dist == "normal"
             mu = rand(mu_dist)
         end
         if dist == "uniform"
             mu = [rand(dims[i]) for i in length(dims)]
         end
-        sigma = eye(d)
+        sigma = eye(d)*scale
         out.components = cat(1,out.components,{GAGauss(d,mu,sigma)})
     end
+    return out
 end
 
 function update(G::GAGauss,x,resp,rate)
@@ -40,7 +42,7 @@ end
 
 function update(M::GAMix,x,rate)
     p_r = {MvNormal(G.mu,G.sigma) for G in M.components};
-    resp = [M.phi[i]*pdf(p_r[i],x) for i=1:p_r]
+    resp = [M.phi[i]*pdf(p_r[i],x) for i=1:length(p_r)]
     resp = resp/sum(resp)
     for i=1:length(M.components)
         update(M.components[i],x,resp[i],rate)
