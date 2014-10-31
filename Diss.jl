1c7a7706dda7bcd380be0bb31848c52342ee9bce
sean <sleepypooka@gmail.com>
2014-10-31 17:04:30 -0400

    Messed with simulations, some fixes for bugs in Diss.jl

--- Diss.jl
+++ Diss.jl
@@ -15,6 +15,7 @@ function getsigma(G::GaussND)
     return G.sigma/(G.nu-G.d-1)
 end
 
+
 # Mixture
 type ItterMix
     d::Int
@@ -25,6 +26,11 @@ type ItterMix
     components
     mean_sigma
     ms_nu
+end
+
+# for pooled covariance
+function getsigma(M::ItterMix)
+    return M.mean_sigma/(M.ms_nu-M.d-1)
 end
 
 # another utility function:
@@ -155,6 +161,7 @@ function update(M::ItterMix,x,comp::Int,reg::Int)
         nu_temp = M.ms_nu/length(M.components)
         mu_temp = M.components[winner].mu
         kappa_temp = M.components[winner].kappa
+        # Not working as it's supposed to
         update(M.components[winner],x,M.mean_sigma/(M.ms_nu-M.d-1),nu_temp);
         M.mean_sigma += ((x-mu_temp)*transpose(x-mu_temp))*kappa_temp/(kappa_temp+1);
         M.ms_nu += 1
@@ -170,13 +177,13 @@ function update(M::ItterMix,x,comp::Int,reg::Int)
             # un-normalize current vector phi (divide by 1-current stick length)
             phi_un = M.phi/(1-M.stick);
             phi_un = cat(1,phi_un,{w_new});
-            alpha_new = (w_new/sum(phi_un))*sum(M.alpha);
+            alpha_new = max((w_new/sum(phi_un))*sum(M.alpha),1);
             M.alpha = cat(1,M.alpha,{alpha_new});
-            sigma_new = M.mean_sigma*(alpha_new);
+            sigma_new = M.mean_sigma*alpha_new;
             M.components = cat(1,M.components,{GaussND(M.d,x,sigma_new,alpha_new+M.d,alpha_new+M.d+M.ms_nu)});
         end
-        
     end
+    M.phi = {M.alpha[i]/sum(M.alpha) for i=1:length(M.alpha)};
     prune = M.phi .> 0.0005;
     M.alpha = M.alpha[prune];
     M.components = M.components[prune];
@@ -198,6 +205,70 @@ function print_mix(M::ItterMix,print_mu=true,print_sigma=true)
             print(M.phi[i])
         end
     end
+end
+
+
+
+
+# Mixture update with regularized Sigma and fixed relationship between pooled and local sigma
+function update(M::ItterMix,x,comp::Int,w::Float64)
+    # determine if new or old category
+    # p(new) decreases as stick-length decreases
+    isnew = rand(Bernoulli(M.stick));
+    
+    # generate posterior predictive densities
+    # currently terribly clunky
+    
+    if !bool(isnew)
+        post_pred = {MvTDist(float(G.nu-M.d+1),G.mu,G.sigma*((G.kappa+1)/(G.kappa*(G.nu-M.d-1)))) for G in M.components};
+        # Calculate and normalize cluster responsibilities
+        resp = [M.phi[i]*pdf(post_pred[i],x) for i=1:length(post_pred)];
+        resp = float(resp/sum(resp));
+        if comp == 1
+            winner = indmax(resp);
+        else
+            winner = indmax(rand(Multinomial(1,resp)));
+        end
+        #nu_temp = M.ms_nu/length(M.components)
+        mu_temp = M.components[winner].mu
+        kappa_temp = M.components[winner].kappa
+        # Not working as it's supposed to
+        update(M.components[winner],x,M.mean_sigma/(M.ms_nu-M.d-1),w);
+        M.mean_sigma += ((x-mu_temp)*transpose(x-mu_temp))*kappa_temp/(kappa_temp+1);
+        M.ms_nu += 1
+        M.alpha[winner] += 1;
+    else
+        w_new = rand(Beta(1,M.a))*M.stick;
+        M.stick = M.stick - w_new;
+        if M.components == {}
+            sigma_new = M.mean_sigma*(M.alpha[1]);
+            M.components = cat(1,M.components,{GaussND(M.d,x,sigma_new,M.alpha[1],M.alpha[1]+M.d+M.ms_nu)});
+        else
+            # Calculating phi for new category:
+            # un-normalize current vector phi (divide by 1-current stick length)
+            phi_un = M.phi/(1-M.stick);
+            phi_un = cat(1,phi_un,{w_new});
+            alpha_new = max((w_new/sum(phi_un))*sum(M.alpha),1);
+            M.alpha = cat(1,M.alpha,{alpha_new});
+            sigma_new = M.mean_sigma*(alpha_new);
+            M.components = cat(1,M.components,{GaussND(M.d,x,sigma_new,alpha_new+M.d,alpha_new+M.d+M.ms_nu)});
+        end
+    end
+    M.phi = {M.alpha[i]/sum(M.alpha) for i=1:length(M.alpha)};
+    prune = M.phi .> 0.0005;
+    M.alpha = M.alpha[prune];
+    M.components = M.components[prune];
+    M.phi = {M.alpha[i]/sum(M.alpha) for i=1:length(M.alpha)};
+end
+
+# Modify update function
+function update(G::GaussND,x,Sigma,w::Float64)
+    conf = [w; 1-w];
+    conf = conf/sum(conf);
+    G.sigma += (((x-G.mu)*transpose(x-G.mu))*conf[1]+Sigma*conf[2])*G.kappa/(G.kappa+1);
+    G.mu = G.mu*(G.kappa/(G.kappa+1)) + x*(1/(G.kappa+1));
+    G.kappa += 1;
+    G.nu += 1;
 end
 
 
